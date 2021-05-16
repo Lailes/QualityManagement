c2b4a48e4c0b993153be1ea3b510366e27b2e336
Auriel <Amadera@protonmail.com>
2021-05-16 13:05:44 +0300

    Add initial code

---
+++ TreeBalance.Shared/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace TreeBalance.Shared {
+    public sealed class Node {
+        
+        public Node(int key) {
+            Key = key;
+            Left = Right = null;
+        }
+        
+        public int Key { get; }
+
+        public int Height {
+            get {
+                var h1 = Left?.Height ?? 0;
+                var h2 = Right?.Height ?? 0;
+                return (h1 > h2 ? h1 : h2) + 1;
+            }
+        }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int BalanceFactor => (Right?.Height ?? 0) - (Left?.Height ?? 0);
+    }
+}
