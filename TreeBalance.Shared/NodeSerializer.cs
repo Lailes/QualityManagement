d70958c3ce70a6c27a4d0f2db3c7328e03061c59
Auriel <Amadera@protonmail.com>
2021-05-17 15:42:52 +0300

    Added Node serialize/deserialize functions #2

---
+++ TreeBalance.Shared/NodeSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace TreeBalance.Shared {
+    public static class NodeSerializer {
+        
+        public static string Serialize(Node node) => 
+            JsonSerializer.Serialize(node, new JsonSerializerOptions{
+                WriteIndented = true
+            });
+
+        public static Node Deserialize(string json) =>
+            JsonSerializer.Deserialize<Node>(json);
+
+    }
+}
