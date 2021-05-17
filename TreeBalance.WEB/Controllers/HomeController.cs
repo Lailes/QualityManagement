d8d4ef70ef45dd30e334de17ae3e3f8bfcffa458
Auriel <Amadera@protonmail.com>
2021-05-17 18:55:31 +0300

    Added WEB UI #6

---
+++ TreeBalance.WEB/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TreeBalance.WEB.Models;
+
+using static TreeBalance.Shared.BalanceExample;
+using static TreeBalance.Shared.NodeSerializer;
+
+namespace TreeBalance.WEB.Controllers {
+    public class HomeController : Controller {
+
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult Index(ParseModel model) {
+            if (model.TreeRequest == null ||  !VerifyTreeString(model.TreeRequest)) {
+                model.TreeResultJson = "Wrong tree line!";
+                return View(model);
+            }
+            var node = CreateNode(model.TreeRequest);
+            model.TreeResultJson = Serialize(model.IsBalance ? Balance(node) : node);
+            return View(model);
+        }
+    }
+}
---
+++ TreeBalance.WEB/Models/ParseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TreeBalance.WEB.Models {
+    public class ParseModel {
+        public bool IsBalance { get; set; }
+        public string TreeRequest { get; set; }
+        public string TreeResultJson { get; set; }
+    }
+}
