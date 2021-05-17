02117b6c78667130ca2335d8928cca8cc2034965
Auriel <Amadera@protonmail.com>
2021-05-17 18:54:27 +0300

    Added REST API #4

---
+++ TreeBalance.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TreeBalance.Shared;
+using static TreeBalance.Shared.BalanceExample;
+
+namespace TreeBalance.API.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class TreeController : ControllerBase {
+
+        [HttpGet("{tree}")]
+        public async Task<ActionResult<Node>> Get(string tree) =>
+            await Task.Run(() => Balance(CreateNode(tree.Replace('-', ' ')))).ConfigureAwait(false);
+    }
+}
